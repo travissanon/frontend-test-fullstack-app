065077c960413b750fa6680b32557634188db493
Travis <travis@devmaterial.com>
2018-10-06 11:18:42 -0400

    [add] first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    products: [],
+    product: {
+      name: 'sample',
+      price: 20,
+    }
+  }
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  getProducts = _ => {
+    fetch('http://localhost:4000/products')
+      .then(response => response.json())
+      .then(response => this.setState({ products: response.data }))
+      .catch(err => console.error(err))
+  }
+
+  addProduct = _ => {
+    const { product } = this.state;
+    fetch(`http://localhost:4000/products/add?name=${product.name}&price=${product.price}`)
+      .then(this.getProducts)
+      .catch(err => console.error(err))
+  }
+
+  renderProduct = ({ product_id, name }) => <div key={product_id}>{name}</div>
+
   render() {
+    const { products, product } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        { products.map(this.renderProduct) }
+        <div>
+          <input 
+            value={product.name} 
+            onChange={e => this.setState({ product: {...product, name: e.target.value} })}
+          />
+          <input 
+            value={product.price} 
+            onChange={e => this.setState({ product: {...product, price: e.target.value} })}
+          />
+
+          <button onClick={this.addProduct}>Add Product</button>
+        </div>
       </div>
     );
   }
